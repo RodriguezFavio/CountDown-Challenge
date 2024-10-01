11770af7a86cff5f754c19dda43a22c002c96541
RodriguezFavio <favio.rodriguez.flores@gmail.com>
2024-10-01 13:20:03 -0300

    Updated to import TimerChallenge component and added instances of it to the challenges div.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import Player from './components/Player.jsx';
+import Player from './components/Player/Player';
+import TimerChallenge from './components/TimerChallenge/TimerChallenge';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Ease" targetTime={1} />
+        <TimerChallenge title="Not ease" targetTime={5} />
+        <TimerChallenge title="Getting tough" targetTime={10} />
+        <TimerChallenge title="Pros only" targetTime={15} />
+      </div>
     </>
   );
 }
